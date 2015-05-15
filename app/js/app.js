766496a12c05a792241c749ae8a469f62cae3884
nina75 <nina150575@gmail.com>
2015-05-15 20:48:32 +0300

    Add routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,71 @@
 'use strict';
 
-var app = angular.module('SocialNetwork', []);
+var app = angular.module('SocialNetwork', ['ngRoute']);
+
+app.config(function ($routeProvider,$locationProvider) {
+    //$locationProvider.html5Mode(true);
+
+    $routeProvider
+        //.when('/login', {
+        //    templateUrl:'templates/login.html',
+        //    controller:'MainController'
+        //})
+        .when('/register', {
+            templateUrl:'partials/register.html',
+            controller:'UserController'
+        })
+        .when('/', {
+            templateUrl:'partials/initial.html'
+        })
+
+        //.when('/user/home', {
+        //    templateUrl:'templates/all-ads.html',
+        //    controller:'MainController'
+        //})
+        //.when('/user/ads', {
+        //    templateUrl:'templates/user-ads.html',
+        //    controller:'MainController'
+        //})
+        //.when('/user/profile', {
+        //    templateUrl:'templates/edit-user.html',
+        //    controller:'MainController'
+        //})
+        //.when('/user/ads/publish', {
+        //    templateUrl:'templates/publish-ad.html',
+        //    controller:'MainController'
+        //})
+        //.when('/user/ads/edit/:id', {
+        //    templateUrl:'templates/edit-ad.html',
+        //    controller:'MainController'
+        //})
+        //.when('/user/ads/delete/:id', {
+        //    templateUrl:'templates/delete-ad.html',
+        //    controller:'MainController'
+        //})
+        //.when('/admin/home', {
+        //    templateUrl:'templates/admin-ads.html',
+        //    controller:'MainController'
+        //})
+        //.when('/admin/ads', {
+        //    templateUrl:'templates/user-ads.html',
+        //    controller:'MainController'
+        //})
+        //.when('/admin/profile', {
+        //    templateUrl:'templates/edit-user.html',
+        //    controller:'MainController'
+        //})
+        //.when('/admin/ads/publish', {
+        //    templateUrl:'templates/publish-ad.html',
+        //    controller:'MainController'
+        //})
+        //.when('/admin/ads/edit/:id', {
+        //    templateUrl:'templates/edit-ad.html',
+        //    controller:'MainController'
+        //})
+        //.when('/admin/ads/delete/:id', {
+        //    templateUrl:'templates/delete-ad.html',
+        //    controller:'MainController'
+        //})
+        //.otherwise({redirectTo: '/'})
+
+});
