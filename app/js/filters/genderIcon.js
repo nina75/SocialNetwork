04c46545a3a00fff95d5fda5e3f3fb83a0fb43c6
nina75 <kozasbrada28>
2015-05-19 21:59:46 +0300

    Gender images

--- app/js/filters/genderIcon.js
+++ app/js/filters/genderIcon.js
@@ -3,9 +3,9 @@
 app.filter('genderIcon' ,function(){
     return function(input){
         switch(input) {
-            case 0: return "img/female-sign-main.jpg";
-            case 1: return "img/female-sign-main.jpg"
-            case 2: return "img/female-sign-main.jpg";
+            case 0: return "img/male-sign-min.png";
+            case 1: return "img/female-sign-min.png"
+            case 2: return "img/other-sign-min.png";
         }
     }
 })
