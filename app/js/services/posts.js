a9fa06c1d44013c966f74d846ab8e6d8703ba48f
nina75 <nina150575@gmail.com>
2015-05-24 05:38:55 +0300

    Code refactoring

--- app/js/services/posts.js
+++ app/js/services/posts.js
@@ -37,7 +37,7 @@ app.factory('posts', function($http, BASE_URL) {
 
     //add new post
     service.addNewPost = function(headers, postData, success, error) {
-        $http.post(BASE_URL + 'posts', postData, {headers: headers})
+        $http.post(BASE_URL + '/posts', postData, {headers: headers})
             .success(function(data) {
                 success(data);
             }).error(function(data) {
